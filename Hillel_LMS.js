ac2da0a78e3fc551c6cd26295d08f1683477b002
Anastasiia <solnce.more.oblaka@gmail.com>
2023-08-23 20:30:39 -0400

    code is added

---
+++ Hillel_LMS.js
@@ -0,0 +1,14 @@
+// Оголошення змінних
+let message = "We are good";
+let word1 = "Hello";
+let word2 = "World";
+let name = "John";
+
+// Виведення слів разом у консоль
+console.log(word1 + " " + word2);
+
+// Виведення типу даних змінної
+console.log(typeof myVariable);
+
+// Виведення рядка з підстановкою змінної
+console.log(`hello ${name}`);
